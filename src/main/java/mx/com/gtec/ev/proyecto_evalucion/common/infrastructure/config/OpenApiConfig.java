ea64b94a7dd0e4c93f204c9cf4f5499286d22927
Gabriel Leos Monroy <gabriel.leos@proveedoresgnp.mx>
2026-05-08 15:37:55 -0600

    feat: add OpenAPI configuration and Swagger UI integration

---
+++ src/main/java/mx/com/gtec/ev/proyecto_evalucion/common/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package mx.com.gtec.ev.proyecto_evalucion.common.infrastructure.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Proyecto Evaluacion")
+                        .version("v1")
+                        .description("Documentacion de endpoints del proyecto")
+                        .contact(new Contact()
+                                .name("Equipo GTEC")
+                                .email("soporte@ejemplo.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
